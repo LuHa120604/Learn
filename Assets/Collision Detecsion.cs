deb172ff7736f5c7f8db933e703b3e4680571d40
Phat <luvinhphat777@gmail.com>
2023-10-30 21:04:13 +0700

    first commit

---
+++ Assets/Collision Detecsion.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetecsion : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTrigerEnter" + other.name);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnTrigerStay" + other.name);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTrigerExit" + other.name);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("OnCollisionEnter");
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("OnCollisionStay" );
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("OnCollisionExit" );
+    }
+}
